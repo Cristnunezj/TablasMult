9bbdffdb194a513fb67430324e66ee87765751f8
Cristnunezj <141673137+Cristnunezj@users.noreply.github.com>
2023-08-08 13:04:19 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs= require('fs');
+const {crearArchivo}= require('./multiplicar')
+console.clear();
+const [,,arg3='base=7']= process.argv;
+const[,base]=arg3.split('=');
+console.log(base);
+
+crearArchivo(base)
+.then(nombreArchivo=>console.log(nombreArchivo,'creado'))
+.catch(err=>console.log(err))
+
